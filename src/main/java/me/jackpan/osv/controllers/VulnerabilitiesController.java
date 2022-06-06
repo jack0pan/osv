2373bdec7577326a1fcd15e7cc4be8bb03130457
Pan Jie <panjie@growingio.com>
2022-06-06 21:05:17 +0800

    feat: add ecosystem and updated since filters

--- src/main/java/me/jackpan/osv/controllers/VulnerabilitiesController.java
+++ src/main/java/me/jackpan/osv/controllers/VulnerabilitiesController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +22,23 @@ public class VulnerabilitiesController {
     private final VulnerabilityRepository vulnerabilityRepository;
 
     @GetMapping(value = "")
-    public Page<Vulnerability> index(@RequestParam Optional<Integer> page, @RequestParam Optional<Integer> size) {
+    public Page<Vulnerability> index(
+            @RequestParam Optional<Integer> page,
+            @RequestParam Optional<Integer> size,
+            @RequestParam Optional<String> ecosystem,
+            @RequestParam Optional<String> updatedSince) {
         Sort sort = Sort.by("updatedAt").descending();
         PageRequest pageRequest = PageRequest.of(page.orElse(0), size.orElse(10), sort);
+        if (ecosystem.isPresent() && updatedSince.isPresent()) {
+
+            return vulnerabilityRepository.findByPackageEcosystemAndUpdatedAtAfter(
+                    ecosystem.get(),
+                    ZonedDateTime.parse(updatedSince.get()),
+                    pageRequest);
+        }
+        if (ecosystem.isPresent()) {
+            return vulnerabilityRepository.findByPackageEcosystem(ecosystem.get(), pageRequest);
+        }
         return vulnerabilityRepository.findAll(pageRequest);
     }
 }
